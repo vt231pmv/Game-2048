f6b52c9be2ca4f8d58d168e8c65d7454f041149f
vt231pmv <vt231_pmv@student.ztu.edu.ua>
2025-02-07 18:32:04 +0200

    переміщені декілька методів у GameBoard

--- Game2048/Game2048/Models/GameBoard.cs
+++ Game2048/Game2048/Models/GameBoard.cs
@@ -9,8 +9,9 @@ namespace Game2048.Models
 {
     public class GameBoard : ViewModel
     {
-        public readonly int boardSize = 4;
+        public readonly int boardSize;
         public readonly int WinValue = 2048;
+        private readonly BoardManipulator manipulator;
 
         public int[,] board;
         public int score;
@@ -18,5 +19,46 @@ namespace Game2048.Models
         public int[,] Board { get => board; set => Set(ref board, value); }
 
         public int Score { get => score; set => Set(ref score, value); }
+
+        public GameBoard(int size = 4)
+        {
+            boardSize = size;
+            board = new int[boardSize, boardSize];
+            manipulator = new BoardManipulator(boardSize);
+            ResetBoard();
+        }
+
+        public void ResetBoard()
+        {
+            board = new int[boardSize, boardSize];
+            score = 0;
+        }
+        public bool Shift(Direction direction)
+        {
+            bool shifted = false;
+
+            for (int i = 0; i < boardSize; i++)
+            {
+                int[] line = manipulator.GetLine(board, i, direction);
+                int[] mergedLine = manipulator.MergeLine(line, ref score);
+                if (!shifted) shifted = !manipulator.AreLinesEqual(line, mergedLine);
+                manipulator.SetLine(board, i, mergedLine, direction);
+            }
+
+            return shifted;
+        }
+        public void AddRandomTile(Random random)
+        {
+            List<(int, int)> emptyCells = new();
+            for (int i = 0; i < boardSize; i++)
+                for (int j = 0; j < boardSize; j++)
+                    if (board[i, j] == 0) emptyCells.Add((i, j));
+
+            if (emptyCells.Count > 0)
+            {
+                var (x, y) = emptyCells[random.Next(emptyCells.Count)];
+                board[x, y] = random.NextDouble() < 0.9 ? 2 : 4;
+            }
+        }
     }
 }
