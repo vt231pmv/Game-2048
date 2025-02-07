65e41288d77f457425634f8b9d747ba66ef8f5dd
vt231pmv <vt231_pmv@student.ztu.edu.ua>
2025-02-07 18:34:56 +0200

    прибраний дублікат коду для Shift

--- Game2048/Game2048/ViewModels/GameViewModel.cs
+++ Game2048/Game2048/ViewModels/GameViewModel.cs
@@ -36,10 +36,10 @@ namespace Game2048.ViewModels
         #region Commands
         public NavigationCommand NavigateToMenuPage { get => new(NavigateToPage, new Uri("Views/Pages/MenuPage.xaml", UriKind.RelativeOrAbsolute)); }
 
-        public RelayCommand ShiftLeftCommand { get => new(ShiftLeft); }
-        public RelayCommand ShiftRightCommand { get => new(ShiftRight); }
-        public RelayCommand ShiftDownCommand { get => new(ShiftDown); }
-        public RelayCommand ShiftUpCommand { get => new(ShiftUp); }
+        public RelayCommand ShiftLeftCommand => new(() => ShiftBoard(Direction.Left));
+        public RelayCommand ShiftRightCommand => new(() => ShiftBoard(Direction.Right));
+        public RelayCommand ShiftDownCommand => new(() => ShiftBoard(Direction.Down));
+        public RelayCommand ShiftUpCommand => new(() => ShiftBoard(Direction.Up));
         public RelayCommand ResetCommand { get => new(Reset); }
         #endregion
 
@@ -174,148 +174,9 @@ namespace Game2048.ViewModels
         #endregion
 
         #region Shifts
-        public void ShiftLeft()
-        {
-            bool shifted = false;
-            for (int i = 0; i < gameBoard.board.GetLength(0); i++)
-            {
-                int index = 0;
-                for (int j = 0; j < gameBoard.board.GetLength(1); j++)
-                {
-                    if (gameBoard.board[i, j] != 0)
-                    {
-                        if (index > 0 && gameBoard.board[i, index - 1] == gameBoard.board[i, j])
-                        {
-                            gameBoard.board[i, index - 1] *= 2;
-                            gameBoard.board[i, j] = 0;
-                            shifted = true;
-                            gameBoard.score += gameBoard.board[i, index - 1];
-                        }
-                        else
-                        {
-                            if (j != index)
-                            {
-                                gameBoard.board[i, index] = gameBoard.board[i, j];
-                                gameBoard.board[i, j] = 0;
-                                shifted = true;
-                            }
-                            index++;
-                        }
-                    }
-                }
-            }
-            if (shifted)
-            {
-                GenerateRandomNumber();
-                CheckGameState();
-            }
-        }
-
-        public void ShiftRight()
-        {
-            bool shifted = false;
-            for (int i = 0; i < gameBoard.board.GetLength(0); i++)
-            {
-                int index = gameBoard.board.GetLength(1) - 1;
-                for (int j = gameBoard.board.GetLength(1) - 1; j >= 0; j--)
-                {
-                    if (gameBoard.board[i, j] != 0)
-                    {
-                        if (index < gameBoard.board.GetLength(1) - 1 && gameBoard.board[i, index + 1] == gameBoard.board[i, j])
-                        {
-                            gameBoard.board[i, index + 1] *= 2;
-                            gameBoard.board[i, j] = 0;
-                            shifted = true;
-                            gameBoard.score += gameBoard.board[i, index + 1];
-                        }
-                        else
-                        {
-                            if (j != index)
-                            {
-                                gameBoard.board[i, index] = gameBoard.board[i, j];
-                                gameBoard.board[i, j] = 0;
-                                shifted = true;
-                            }
-                            index--;
-                        }
-                    }
-                }
-            }
-            if (shifted)
-            {
-                GenerateRandomNumber();
-                CheckGameState();
-            }
-        }
-
-        public void ShiftDown()
-        {
-            bool shifted = false;
-            for (int j = 0; j < gameBoard.board.GetLength(1); j++)
-            {
-                int index = gameBoard.board.GetLength(0) - 1;
-                for (int i = gameBoard.board.GetLength(0) - 1; i >= 0; i--)
-                {
-                    if (gameBoard.board[i, j] != 0)
-                    {
-                        if (index < gameBoard.board.GetLength(0) - 1 && gameBoard.board[index + 1, j] == gameBoard.board[i, j])
-                        {
-                            gameBoard.board[index + 1, j] *= 2;
-                            gameBoard.board[i, j] = 0;
-                            shifted = true;
-                            gameBoard.score += gameBoard.board[index + 1, j];
-                        }
-                        else
-                        {
-                            if (i != index)
-                            {
-                                gameBoard.board[index, j] = gameBoard.board[i, j];
-                                gameBoard.board[i, j] = 0;
-                                shifted = true;
-                            }
-                            index--;
-                        }
-                    }
-                }
-            }
-            if (shifted)
-            {
-                GenerateRandomNumber();
-                CheckGameState();
-            }
-        }
-
-        public void ShiftUp()
-        {
-            bool shifted = false;
-            for (int j = 0; j < gameBoard.board.GetLength(1); j++)
-            {
-                int index = 0;
-                for (int i = 0; i < gameBoard.board.GetLength(0); i++)
-                {
-                    if (gameBoard.board[i, j] != 0)
-                    {
-                        if (index > 0 && gameBoard.board[index - 1, j] == gameBoard.board[i, j])
-                        {
-                            gameBoard.board[index - 1, j] *= 2;
-                            gameBoard.board[i, j] = 0;
-                            shifted = true;
-                            gameBoard.score += gameBoard.board[index - 1, j];
-                        }
-                        else
-                        {
-                            if (i != index)
-                            {
-                                gameBoard.board[index, j] = gameBoard.board[i, j];
-                                gameBoard.board[i, j] = 0;
-                                shifted = true;
-                            }
-                            index++;
-                        }
-                    }
-                }
-            }
-            if (shifted)
+        private void ShiftBoard(Direction direction)
+        { 
+            if (gameBoard.Shift(direction))
             {
                 GenerateRandomNumber();
                 CheckGameState();
