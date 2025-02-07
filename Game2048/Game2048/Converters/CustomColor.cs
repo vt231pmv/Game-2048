c800fe119cf171d4fe4f2d1798e02b762dd21150
vt231pmv <vt231_pmv@student.ztu.edu.ua>
2025-02-07 18:17:51 +0200

    створений клас CustomColor для збереження кольору у форматі rgb

---
+++ Game2048/Game2048/Converters/CustomColor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game2048.Converters
+{
+    public class CustomColor
+    {
+        public byte Red { get; private set; }
+        public byte Green { get; private set; }
+        public byte Blue { get; private set; }
+        public CustomColor(byte r, byte g, byte b)
+        {
+            Red = r;
+            Green = g;
+            Blue = b;
+        }
+    }
+}
